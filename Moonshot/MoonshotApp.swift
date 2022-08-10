309865dfd7cb316761fee69cf83834414eaef4fb
Jiaming Guo <j23guo@gmail.com>
2022-08-10 09:18:14 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jiaming Guo on 2022-08-10.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
